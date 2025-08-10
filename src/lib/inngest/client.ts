3e3e9355a0f2b78c1ea39d4eea4b2e6736753a08
Ajuuke <asarajoog@gmail.com>
2025-08-10 18:05:07 -0400

    Updated the eventKey

--- src/lib/inngest/client.ts
+++ src/lib/inngest/client.ts
@@ -3,4 +3,6 @@ import { Inngest } from 'inngest'
 // Create and export the Inngest client
 export const inngest = new Inngest({
   id: 'e-ticket-system',
+  eventKey:
+    'signkey-prod-2c594f895049eb276a7a9f18178961c57b8a741bb9ae37dbc666c0f4f77305d1', // From Inngest dashboard
 })
